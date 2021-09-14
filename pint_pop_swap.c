b9afc668049d0941ced20b661b105b7bfc8a924d
1107tomkim <cryptotaekim@gmail.com>
2021-09-14 21:23:03 +0000

    Adding pint and pop

--- pint_pop_swap.c
+++ pint_pop_swap.c
@@ -8,14 +8,40 @@
 **/
 void pint(stack_t **stack, unsigned int line_number)
 {
-
+	if (stack == NULL && *stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", *stack->n);
 }
 
+/**
+ *
+ *
+ */
 void pop(stack_t **stack, unsigned int line_number)
 {
+	stack_t *tmp;
 
+	tmp=*stack;
+	if (tmp == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if (tmp->next = NULL)
+	{
+		*stack = NULL;
+		free(tmp);
+	}
 }
 
+/**
+ *
+ *
+ *
+ */
 void swap(stack_t **stack, unsigned int line_number)
 {
 
