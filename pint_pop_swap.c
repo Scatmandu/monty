d2501ce676656043198ecb8d7d8c6a59401af056
1107tomkim <cryptotaekim@gmail.com>
2021-09-15 01:56:39 +0000

    Reverting back to original

--- pint_pop_swap.c
+++ pint_pop_swap.c
@@ -12,6 +12,9 @@ void pint(stack_t **stack, unsigned int line_number, char *opcode, FILE *fd)
 	if (stack == NULL || *stack == NULL)
 	{
 		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		free_list(*stack);
+		free(opcode);
+		fclose(fd);
 		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", (*stack)->n);
